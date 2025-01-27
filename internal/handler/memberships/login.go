f6e652973410253e90fc2e06349c1a306b88a488
Ahmad Indra Nurfauzi <ahmad.nurfauzi@moladin.com>
2025-01-27 17:32:44 +0700

    implement login

---
+++ internal/handler/memberships/login.go
@@ -0,0 +1,30 @@
+package memberships
+
+import (
+	"net/http"
+
+	"github.com/IndraNurfa/music-catalog/internal/models/memberships"
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) Login(c *gin.Context) {
+	var request memberships.LoginRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	accessToken, err := h.service.Login(request)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, memberships.LoginResponse{
+		AccessToken: accessToken,
+	})
+}
